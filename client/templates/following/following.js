88de717b223fe0e1d9993a261d5288a5267615a5
stuarttiedemann <stuarttiedemann@yahoo.com>
2016-03-14 16:20:56 -0500

    Removed insecure package and added methods to improve security.

--- client/templates/following/following.js
+++ client/templates/following/following.js
@@ -1,5 +1,9 @@
 Template.following.helpers({
+	isOwner: function(){
+		return this.owner === Meteor.userId();
+	},
 	posts: function(){
-		return Liked.find({},{sort:{createdAt:-1}});
+
+		return Liked.find({likedBy:Meteor.userId()},{sort:{createdAt:-1}});
 	}
 });
--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -7,64 +7,33 @@ Template.home.helpers({
 	}
 });
 
-
-
 Template.home.events({
 	'submit .submit-posts': function(event){
 		var newPost = event.target.newPost.value;
-
-		Posts.insert({
-			newPosts: newPost,
-			count: 0,
-			createdAt: new Date(),
-			owner: Meteor.userId()
-		});
+		Meteor.call("addPost",newPost, Meteor.userId());
 		event.target.newPost.value = '';
 		return false;
 	},
 	'click .toggle-checked': function(){
+		var id = this._id;
+		var setChecked = !this.checked;
 		var task = Posts.findOne(this._id);
-		console.log(task._id);
-		Posts.update(this._id,{$set:{checked:!this.checked}});
-		
 		var isItChecked = $(event.target).prop('checked');
-		console.log(isItChecked);
-		if(isItChecked == true){
-			Liked.insert({
-				postId: this._id,
-				likedBy: Meteor.userId(),
-				newPosts:this.newPosts,
-				createdAt: new Date()
-			});
-			
-		}else{
-				console.log(task._id);
-				var doc = Liked.findOne({postId:task._id});
-				Liked.remove({_id:doc._id});
-				
-			}
-	},
-	'click .liked': function(){
-
+		var newFollowedPosts = this.newPosts;
+	
+		Meteor.call('followPost', id, task, isItChecked, newFollowedPosts, setChecked );
 	},
 	'click .delete': function(){
-		Posts.remove(this._id);
+		Meteor.call("deletePost", this._id);
 	},
 	'click .thumbs-up': function(){
-		if(this.owner !== Meteor.userId()){
-			Posts.update(this._id,{$inc:{count:+1}});
-			event.target.classList.add('voted-up');
-			
-		}else{
-			FlashMessages.sendWarning("You Can't Like Your Own Post");
-		}
+		var owner = this.owner;
+		var id = this._id;
+		Meteor.call('thumbsUp', owner, id);
 	},
 	'click .thumbs-down': function(){
-		if(this.owner !== Meteor.userId()){
-			Posts.update(this._id,{$inc:{count:-1}});
-			
-		}else{
-			FlashMessages.sendWarning("You Can't Like Your Own Post");
-		}
+		var owner = this.owner;
+		var id = this._id;
+		Meteor.call('thumbsDown', owner, id);
 	}
 });
